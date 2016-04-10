769b2f936242cc3080ba591de1b3ff216f58f776
Michael Vorburger <mike@vorburger.ch>
2016-04-10 23:58:31 +0200

    Bug 491400 fix attempt - failed, still NOK

---
+++ web/org.eclipse.xtext.web/src/main/java/org/eclipse/xtext/web/server/EmptyClassDueToGRADLE1288bug.java
@@ -0,0 +1,9 @@
+package org.eclipse.xtext.web.server;
+
+/**
+ * Empty Java (not Xtend) class required as a workaround for a Gradle bug in build.
+ * @see <a href="https://issues.gradle.org/browse/GRADLE-1288">GRADLE-1288 bug</a>
+ * @author Michael Vorburger
+ */
+public class EmptyClassDueToGRADLE1288bug {
+}
